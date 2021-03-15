ad24a764783cd1ef945870613c492e2ce1e6a334
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-15 19:23:37 -0400

    Actions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // npm install --yes // install package.json
 // npm install redux
-console.log('From index.js');
+const BUY_CAKE = 'BUY_CAKE';
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action',
+  };
+}
